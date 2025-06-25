c55b513d57152498382fe4243b4bdf56953ea390
hategan19 <hateganmatei8@gmail.com>
2025-06-25 11:43:04 +0300

    Add files via upload

---
+++ program .net (Hategan Matei 24 iun).cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("name: ");
+            Console.WriteLine("helllo," + (char)Console.Read());
+        }
+    }
+}
---
+++ tema 1 (Hategan Matei 24 iun).cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a numeric value for a: ");
+int a;
+a = 34;
+Console.WriteLine("a is now " + a);
+Console.WriteLine("New value for a: ");
+a=int.Parse(Console.ReadLine());
+Console.WriteLine("a is now " + a);
+Console.WriteLine("Test if a is an integer");
+string? input = Console.ReadLine();
+if (int.TryParse(input, out int number))
+    Console.WriteLine("a is an integer");
+else
+    Console.WriteLine("a is not an integer");
+
+Console.WriteLine("If a is an integer, these are its minimum and maximum values:");
+Console.WriteLine("Min: " + int.MinValue);
+Console.WriteLine("Max: " + int.MaxValue);
+
+Console.WriteLine("Test if a is a number with decimals");
+string? input2= Console.ReadLine();
+if (double.TryParse(input2, out double number2))
+    Console.WriteLine("a is a number with decimals");
+else
+    Console.WriteLine("a is not a number with decimals");
+
+Console.WriteLine("If a is a double, these are its minimum and maximum values:");
+Console.WriteLine("Min: " + double.MinValue);
+Console.WriteLine("Max: " + double.MaxValue);
+
+Console.WriteLine("Now, test if a is a character");
+string? input3 = Console.ReadLine();
+if (char.TryParse(input3, out char character))
+    Console.WriteLine("a is a character");
+else
+    Console.WriteLine("a is not a character");
+
+Console.WriteLine("If a is a character, these are its minimum and maximum values, in ASCII code:");
+Console.WriteLine("Min: " + (int)char.MinValue);
+Console.WriteLine("Max: " + (int)char.MaxValue);
